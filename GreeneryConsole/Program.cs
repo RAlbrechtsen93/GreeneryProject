8bf9fa07939f81a2b75ec30ef22e6d39d3701d98
R_Alb <r_albrechtsen@outlook.com>
2016-12-09 10:52:04 +0100

    Added GreeneryConsole

---
+++ GreeneryConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GreeneryConsole.ServiceReference1;
+
+namespace GreeneryConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service1Client client = new Service1Client("BasicHttpBinding_IService1");
+
+            var getalldata = client.GetAllData();
+            Console.WriteLine(getalldata);
+
+            Console.ReadKey();
+        }
+    }
+}
